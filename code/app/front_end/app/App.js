161c8aa8530b962072efac95ef8b07e7d2fb136d
OLAOOT <furin1998@gmail.com>
2023-07-10 16:29:31 +0900

    feat(App.js): add root view
    - Add BottomTab (NavBar)
    - Add rough router
    
    Issue #1

--- code/app/front_end/app/App.js
+++ code/app/front_end/app/App.js
@@ -1,14 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, View, Text, ActivityIndicator } from 'react-native';
+import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+// Screen Components
+const HomeScreen = () => <Text>first</Text>;
+const Screen1 = () => <Text>screen 1</Text>;
+const Screen2 = () => <Text>screen 2</Text>;
+
+// Loading Component
+const LoadingScreen = () => (
+  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <ActivityIndicator size="large" />
+    <Text>Loading...</Text>
+  </View>
+);
+
+// Tab Navigator
+// const Tab = createBottomTabNavigator();
+
+// Main Stack Navigator
+// const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator();
+const BottomTab = createBottomTabNavigator();
+
+const BottomTabNavigator = () => {
   return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <BottomTab.Navigator initialRouteName="Screen1">
+      <BottomTab.Screen name="Screen1" component={Screen1} />
+      <BottomTab.Screen name="Screen2" component={Screen2} />
+    </BottomTab.Navigator>
+  )
 }
+
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulating loading time
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }, []);
+
+  if (isLoading) {
+    return <LoadingScreen />;
+  }
+
+  return (
+    <NavigationContainer
+      theme={DefaultTheme}>
+      <Stack.Navigator initialRouteName='Screen1'>
+        <Stack.Screen name="Root" component={BottomTabNavigator} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
+
+
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Hello World</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
 
 const styles = StyleSheet.create({
   container: {
