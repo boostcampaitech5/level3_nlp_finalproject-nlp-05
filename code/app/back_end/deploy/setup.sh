ae7dfed80f94f85824bc67411648f27568e5fe16
Seoboseong <sboseong124@gmail.com>
2023-07-11 18:56:04 +0000

    fix(setup): fix setup files for docker building
    - fix setup.sh
    - fix settins.py
    
    Issue #2

--- code/app/back_end/deploy/setup.sh
+++ code/app/back_end/deploy/setup.sh
@@ -3,8 +3,8 @@
 set -e
 
 apt-get update
-apt-get install -y python3-dev python3-venv sqlite3 supervisor nginx git
-apt-get install build-essential python
+apt-get install -y python3-dev python3-venv sqlite3 supervisor nginx git systemd
+apt-get install build-essential python3
 
 PROJECT_GIT_URL='https://github.com/boostcampaitech5/level3_nlp_finalproject-nlp-05.git'
 PROJECT_BASE_PATH='/code/server'
@@ -17,13 +17,16 @@ python3 -m venv $PROJECT_BASE_PATH/code/app/back_end/env
 
 cd server
 git remote set-url origin https://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@github.com/boostcampaitech5/level3_nlp_finalproject-nlp-05.git
+git pull origin feat/back_end_init
 
-$PROJECT_BASE_PATH/code/app/back_end/env/bin/pip install -r /code/requirements.txt uwsgi==2.0.21
+$PROJECT_BASE_PATH/code/app/back_end/env/bin/pip install -r /code/requirement.txt uwsgi==2.0.21
 
 $PROJECT_BASE_PATH/code/app/back_end/env/bin/python $PROJECT_BASE_PATH/code/app/back_end/manage.py migrate
 $PROJECT_BASE_PATH/code/app/back_end/env/bin/python $PROJECT_BASE_PATH/code/app/back_end/manage.py collectstatic --noinput
 
 cp $PROJECT_BASE_PATH/code/app/back_end/deploy/supervisor_fine_api.conf /etc/supervisor/conf.d/fine_api.conf
+service supervisor stop
+service supervisor start 
 supervisorctl reread
 supervisorctl update
 supervisorctl restart fine_api
