e272ae396991ea7047a81d96c6ba485add4d4585
OLAOOT <furin1998@gmail.com>
2023-07-22 03:38:59 +0900

    refactor(Loading): Refactor loading flow
    - Remove Login.js, Add Loading.js
    - move loading font to Loading.js

--- code/app/front_end/app/screens/Loading.js
+++ code/app/front_end/app/screens/Loading.js
@@ -1,16 +1,22 @@
 import React, { useEffect, useContext } from 'react';
-import styled from 'styled-components/native';
+import * as Font from 'expo-font';
 import { Context } from '../utils/Context';
 import * as SecureStore from 'expo-secure-store';
 import { toast } from '../utils/toast';
 import * as SplashScreen from 'expo-splash-screen';
 
-const Login = () => {
+const Loading = () => {
 	const { setIsLogin, setUserId, setFirstVisit } = useContext(Context);
 
 	useEffect(() => {
-		const handleLogin = async () => {
+		const loading = async () => {
 			try {
+				// load font
+				await Font.loadAsync({
+					'HandWriting': require('../assets/fonts/nanum_handwriting.ttf')
+				});
+
+				// login or signup
 				const savedUserId = await SecureStore.getItemAsync('userId');
 				if (savedUserId) {
 					setUserId(savedUserId);
@@ -29,15 +35,17 @@ const Login = () => {
 			} finally {
 				setTimeout(async () => {
 					setIsLogin(true);
+
+					// hide splash screen
 					await SplashScreen.hideAsync();
 				}, 1000);
 			}
 		};
 		
-		handleLogin();
+		loading();
 	}, []);
 
 	return null;
 };
 
-export default Login;
+export default Loading;
--- code/app/front_end/app/screens/Main.js
+++ code/app/front_end/app/screens/Main.js
@@ -1,28 +1,16 @@
-import React, { useEffect, useContext } from 'react';
-import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Context } from '../utils/Context'
-import Login from './Login'
 import TabNavigator from '../components/TabNavigator';
 
 const Stack = createNativeStackNavigator();
 
-const Main = () => {
-	const { isLogin } = useContext(Context);
-
-	return (
-		<>
-			{isLogin ? (
-					<NavigationContainer
-						theme={DefaultTheme}>
-						<Stack.Navigator initialRouteName='ChatBot'>
-							<Stack.Screen name='Root' component={TabNavigator} options={{headerShown: false}} />
-						</Stack.Navigator>
-					</NavigationContainer>
-				) : <Login />
-			}
-		</>
-	);
-};
+const Main = () => (
+	<NavigationContainer>
+		<Stack.Navigator initialRouteName='ChatBot'>
+			<Stack.Screen name='Root' component={TabNavigator} options={{headerShown: false}} />
+		</Stack.Navigator>
+	</NavigationContainer>
+);
 
 export default Main;
